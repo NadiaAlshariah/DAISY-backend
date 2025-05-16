484e57fef4c27967301ffc3aba2697d5d8c6514b
NadiaAlshariah <nadiakalshariah@gmail.com>
2025-05-16 19:15:07 +0300

    auth frontend update

--- app/services/UserService.py
+++ app/services/UserService.py
@@ -2,30 +2,30 @@ from app.database import mongo
 from app.models.User import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo.errors import DuplicateKeyError
-from werkzeug.exceptions import Conflict
+from app.exception.BadRequestException import BadRequestException 
 
 class UserService:
     @staticmethod
     def create_user(username: str, email: str, password: str):
-        try:
-            if UserService.find_by_email(email) or UserService.find_by_username(username):
-                raise Conflict("Email or username already exists")
+        if UserService.find_by_email(email):
+            raise BadRequestException("email already exists")
+        
+        if UserService.find_by_username(username):
+            raise BadRequestException("username already exists")
 
-            hashed_password = generate_password_hash(password)
-            user = User(username, email, hashed_password)
+        hashed_password = generate_password_hash(password)
+        user = User(username=username, email=email, password=hashed_password)
             
-            result = UserService.save(user)
-            return result
-
-        except DuplicateKeyError:
-            raise Conflict("Email or username already exists")
+        result = UserService.save(user)
+        return result
+     
      
     @staticmethod
     def save(user: User):
         user_data = {
             "username": user.username,
             "email": user.email,
-            "password": user.password_hash, 
+            "password": user.password, 
         }
         if user.id:
             mongo.db.users.update_one({"_id": user.id}, {"$set": user_data})
@@ -34,16 +34,26 @@ class UserService:
             user._id = result.inserted_id
         return user._id
 
+
     @staticmethod
     def find_by_email(email: str) -> User:
         user_data = mongo.db.users.find_one({"email": email})
-        return User(user_data) if user_data else None
+
+        if user_data:
+            user_data["id"] = str(user_data.pop("_id"))
+            return User(**user_data)
+        
+        return None
     
     
     @staticmethod
     def find_by_username(username: str) -> User:
         user_data = mongo.db.users.find_one({"username": username})
-        return User(user_data) if user_data else None
+        if user_data:
+            user_data["id"] = str(user_data.pop("_id"))
+            return User(**user_data)
+        
+        return None
     
     @staticmethod
     def verify_password(hashedPassword ,password) -> bool:
