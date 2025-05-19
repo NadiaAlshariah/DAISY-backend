c9c599031a1d4f9669ee5a1957259c390a4b3ce6
NadiaAlshariah <nadiakalshariah@gmail.com>
2025-05-19 13:34:00 +0300

    merge crop and block

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from .database import init_db, mongo
 from flask_jwt_extended import JWTManager
 from .routes.AuthRoute import auth_bp
 from app.routes.LandRoute import land_bp
-from app.routes.CropRoute import crop_bp
 from app.routes.BlockRoute import block_bp
 from flask import Flask, jsonify
 from app.exception.BadRequestException import BadRequestException
@@ -22,7 +21,6 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(land_bp)
     app.register_blueprint(block_bp)
-    app.register_blueprint(crop_bp)
 
     # Register Exception Handlers
     @app.errorhandler(BadRequestException)
--- app/models/Block.py
+++ app/models/Block.py
@@ -2,14 +2,20 @@ from app.enum.DrainagePropertiesEnum import DrainagePropertiesEnum
 from pydantic import BaseModel
 from app.enum.SoilTypeEnum import SoilType
 from typing import Optional
+from datetime import datetime
+from app.enum.CropTypeEnum import CropType
+from app.enum.GrowthStateEnum import GrowthState
 
 
 class Block(BaseModel):
     id: Optional[str] = None
-    location: str
     land_id: str
     current_soil_moisture: float
     soil_type: SoilType
     water_retention_capacity: float  
     drainage_properties: DrainagePropertiesEnum
-    #each block has one plant ?
+    crop_type: CropType  
+    growth_state: GrowthState
+    block_id: str
+    planted_at: datetime = datetime.now()
+    crop_water_requirement: float
