60b7015ca0d915eed2c28adde5cbb16f038ab2a5
LeenJaber1 <jaberleen95@gmail.com>
2025-05-19 20:18:14 +0300

    weather api connection done

--- app/config.py
+++ app/config.py
@@ -9,3 +9,4 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES")))
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES")))
+    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
